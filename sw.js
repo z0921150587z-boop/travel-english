49a4b112422a691e51fd52ae2c5720af51b3c236
z0921150587z-boop <z0921150587z@gmail.com>
2026-09-25 16:51:13 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VER='v8';
+const VER='v9';
 const CACHE='travel-english-'+VER;
 const ASSETS=['./','./index.html?'+VER,'./styles.css?'+VER,'./config.js?'+VER,'./data.js?'+VER,'./dest.js?'+VER,'./engine.js?'+VER,'./app.js?'+VER,'./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
